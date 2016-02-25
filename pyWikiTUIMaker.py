9f8b7a752afa2daa1d8ab5217206d28c269931b8
Halil Gamsızkan <halilgamsizkan@gmail.com>
2016-02-25 14:21:53 +0200

    fixes

--- pyWikiTUIMaker.py
+++ pyWikiTUIMaker.py
@@ -10,34 +10,33 @@ class widget(object):
     def __init__(self, name, win):
         self.name=name
         self.label=""
-        self.posX=1
-        self.posY=1
-        self.win=win
-        self.fieldSize=10
         self.win=win
 
     def printLabel(self):
         self.win.addstr(self.posY,self.posX, self.label)
-
-    def moveTo(self, y, x):
-        self.win.addstr(y,x,'')
         
 
 class wTextBox(widget):
     
-    def __init__(self, name, win):
+    def __init__(self, name, win, y, x, size, label):
         super(self.__class__,self).__init__(name, win) # Call the init method of the base class
+        self.posX=x
+        self.posY=y
+        self.fieldSize=size
+        self.label=label
         type="t"
-         # stdscr.subwin(23, 79, 0, 0)
-
+        self.swin = win.subwin(1, self.fieldSize, self.posY,self.posX + 3 + len(self.label)) #nlines, ncols, begin_y, begin_x
+        # self.swin.bkgd(' ', curses.color_pair(1))
+        self.win.refresh()
+        self.swin.refresh()
     def printField(self):
         self.win.addstr(self.posY,self.posX + 2 + len(self.label), "[")
         self.win.addstr(self.posY,self.posX + 2 + len(self.label) + self.fieldSize, "]")
         
-    def getFocus(self):
-        self.moveTo(self.posY,self.posX + 3 + len(self.label))
-        text=textpad.Textbox(self.win, insert_mode=True).edit()
-        curses.addstr(10,1,text.encode('utf_8'))
+    def setFocus(self):
+        self.swin.move(0,0)#(self.posY,self.posX + 3 + len(self.label))
+        mytext=textpad.Textbox(self.swin, insert_mode=True).edit()
+        # self.win.addstr(10,1,mytext)
         
 
 def init():
@@ -45,8 +44,9 @@ def init():
     isColorTerm=True
     # print curses.has_colors()
     # sys.exit()
+    curses.start_color() 
     try:
-        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_BLUE)
+        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
         stdscr.bkgd(' ', curses.color_pair(1))
     except curses.error:
         print "color not available. set TERM=xterm-color"
@@ -70,17 +70,14 @@ def draw_screen(win):
 
 def define_widgets(win):
     widgets = []
-    t0=wTextBox("t0", win)
-    t0.label="Name"
-    t0.posY=2    
-    t0.posX=7
+    t0=wTextBox("t0", win, 2, 7, 10, "Name")
     
     widgets.append(t0)
     return widgets
      
 def main(stdscr):
     # Frame the interface area at fixed VT100 size
-    screen = stdscr.subwin(23, 79, 0, 0)
+    screen = stdscr.subwin(0, 0, 0, 0)#(23, 79, 0, 0)
     screen.box()
     # screen.hline(2, 1, curses.ACS_HLINE, 77)
     draw_screen(screen)
@@ -88,8 +85,9 @@ def main(stdscr):
     for widget in widgets:
         widget.printLabel()
         widget.printField()
-    widgets[0].getFocus()
-    screen.refresh()
+        screen.refresh()
+    widgets[0].setFocus()
+    
     while True:
         c = screen.getch()
         print c
